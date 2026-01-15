23d21bdb52c134c3721cb680b9554fcf5e1ff03c
bollulu <senol_erk_91@hotmail.com>
2026-01-15 04:45:47 +0300

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import eventlet
-# 'all=True' parametresi Render'da aldığın bloklama hatasını çözen anahtardır.
-# MUTLAKA dosyanın en üstünde, diğer tüm kütüphanelerden önce gelmelidir.
-eventlet.monkey_patch(all=True)
+eventlet.monkey_patch(all=True)  # Kritik: En üstte olmalı!
 
 import os
 from flask import Flask, render_template, request, redirect, url_for
@@ -10,24 +8,21 @@ from flask_login import LoginManager, UserMixin, login_user, logout_user, login_
 from flask_socketio import SocketIO, emit
 from datetime import datetime
 
-# Uygulama Yapılandırması
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'final-secure-key-2026'
+app.config['SECRET_KEY'] = 'chat-plus-secure-2026'
 
-# Veritabanı Yolu (Render/SQLite uyumluluğu için absolute path)
+# SQLite Veritabanı Yolu (Render için tam yol)
 basedir = os.path.abspath(os.path.dirname(__file__))
-db_path = os.path.join(basedir, 'chat_data.db')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'chat_data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-# Render için eventlet modu zorunludur. cors_allowed_origins="*" bağlantı hatalarını önler.
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet', logger=False, engineio_logger=False)
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
-# Modeller
+# Veritabanı Modelleri
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
@@ -35,7 +30,6 @@ class User(UserMixin, db.Model):
     avatar = db.Column(db.Text, nullable=True, default='')
 
 class Message(db.Model):
-    id = db.Column(#ff0000)
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
     content = db.Column(db.Text, nullable=False)
@@ -48,17 +42,14 @@ def load_user(user_id):
 
 # Rotalar
 @app.route('/')
-def index():
-    return redirect(url_for('login'))
+def index(): return redirect(url_for('login'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        uname = request.form.get('username')
-        pwd = request.form.get('password')
-        if uname and pwd and not User.query.filter_by(username=uname).first():
-            new_user = User(username=uname, password=pwd)
-            db.session.add(new_user)
+        u, p = request.form.get('username'), request.form.get('password')
+        if u and p and not User.query.filter_by(username=u).first():
+            db.session.add(User(username=u, password=p))
             db.session.commit()
             return redirect(url_for('login'))
     return render_template('register.html')
@@ -75,13 +66,13 @@ def login():
 @app.route('/chat')
 @login_required
 def chat():
-    all_messages = Message.query.all()
+    msgs = Message.query.all()
     history = []
-    for m in all_messages:
+    for m in msgs:
         sender = User.query.filter_by(username=m.username).first()
         history.append({
-            'id': m.id, 'username': m.username, 'content': m.content, 
-            'type': m.type, 'timestamp': m.timestamp, 
+            'id': m.id, 'username': m.username, 'content': m.content,
+            'type': m.type, 'timestamp': m.timestamp,
             'avatar': sender.avatar if sender else ''
         })
     return render_template('chat.html', user=current_user, history=history)
@@ -109,22 +100,18 @@ def disconnect():
 def handle_msg(data):
     if not current_user.is_authenticated: return
     now = datetime.now().strftime("%H:%M")
-    m_type = data.get('type', 'text')
-    new_m = Message(username=current_user.username, content=data['msg'], type=m_type, timestamp=now)
-    db.session.add(new_m)
-    db.session.commit()
-    
+    new_m = Message(username=current_user.username, content=data['msg'], type=data.get('type', 'text'), timestamp=now)
+    db.session.add(new_m); db.session.commit()
     emit('message', {
-        'id': new_m.id, 'user': current_user.username, 'msg': data['msg'], 
-        'time': now, 'type': m_type, 'avatar': current_user.avatar or ''
+        'id': new_m.id, 'user': current_user.username, 'msg': data['msg'],
+        'time': now, 'type': data.get('type', 'text'), 'avatar': current_user.avatar or ''
     }, broadcast=True)
 
 @socketio.on('delete_message')
 def delete_msg(data):
     msg = Message.query.get(data['id'])
     if msg and msg.username == current_user.username:
-        db.session.delete(msg)
-        db.session.commit()
+        db.session.delete(msg); db.session.commit()
         emit('remove_message', {'id': data['id']}, broadcast=True)
 
 @socketio.on('update_avatar')
@@ -136,10 +123,8 @@ def update_avatar(data):
         if info['name'] == user.username: info['avatar'] = data['img']
     emit('user_list', list(active_users.values()), broadcast=True)
 
-# Sunucu Başlatma
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     port = int(os.environ.get("PORT", 5000))
-    # 'log_output=False' Render loglarını temiz tutar ve bloklamayı azaltır.
     socketio.run(app, host='0.0.0.0', port=port, debug=False, log_output=False)
