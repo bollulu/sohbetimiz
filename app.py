35838479b054b514d6980e8f87717dba8c7bfda1
bollulu <senol_erk_91@hotmail.com>
2026-01-15 07:59:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ from flask_socketio import SocketIO, emit, join_room
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'sohbetimiz-2026-final'
+app.config['SECRET_KEY'] = 'sohbetimiz-ultra-2026'
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'final_v3.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent', max_http_buffer_size=50 * 1024 * 1024) # 50MB dosya desteği
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
@@ -23,13 +23,14 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-    avatar = db.Column(db.Text, nullable=True, default='')
+    avatar = db.Column(db.Text, nullable=True, default='https://www.w3schools.com/howto/img_avatar.png')
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     room = db.Column(db.String(50), default='Genel')
     username = db.Column(db.String(50))
-    content = db.Column(db.Text)
+    content = db.Column(db.Text) # Base64 veri veya metin
+    msg_type = db.Column(db.String(10), default='text') # text, image, video
     timestamp = db.Column(db.String(10))
 
 with app.app_context():
@@ -69,23 +70,23 @@ def live(): return render_template('live.html', user=current_user)
 @app.route('/logout')
 def logout(): logout_user(); return redirect(url_for('login'))
 
-# Socket İşlemleri
 @socketio.on('join')
 def on_join(data):
     room = data.get('room', 'Genel')
     join_room(room)
     session['room'] = room
-    msgs = Message.query.filter_by(room=room).order_by(Message.id.desc()).limit(50).all()
-    history = [{'id': m.id, 'user': m.username, 'msg': m.content, 'time': m.timestamp} for m in reversed(msgs)]
+    msgs = Message.query.filter_by(room=room).order_by(Message.id.desc()).limit(20).all()
+    history = [{'id': m.id, 'user': m.username, 'msg': m.content, 'type': m.msg_type, 'time': m.timestamp} for m in reversed(msgs)]
     emit('history', history)
 
 @socketio.on('message')
 def handle_msg(data):
     room = session.get('room', 'Genel')
     now = datetime.now().strftime("%H:%M")
-    msg = Message(username=current_user.username, content=data['msg'], room=room, timestamp=now)
+    m_type = data.get('type', 'text')
+    msg = Message(username=current_user.username, content=data['msg'], msg_type=m_type, room=room, timestamp=now)
     db.session.add(msg); db.session.commit()
-    emit('message', {'id': msg.id, 'user': current_user.username, 'msg': data['msg'], 'time': now, 'avatar': current_user.avatar}, to=room)
+    emit('message', {'id': msg.id, 'user': current_user.username, 'msg': data['msg'], 'type': m_type, 'time': now, 'avatar': current_user.avatar}, to=room)
 
 @socketio.on('delete_message')
 def delete_msg(data):
@@ -101,7 +102,6 @@ def update_avatar(data):
     user.avatar = data['img']
     db.session.commit()
 
-# Canlı Yayın Sinyalleşme
 @socketio.on('join_live')
 def handle_join_live(data):
     join_room('live_room')
