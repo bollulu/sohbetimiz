c86aeb5c0df9985ef59ac837cb3e4f6d0039dcb0
bollulu <senol_erk_91@hotmail.com>
2026-01-15 04:42:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import eventlet
-# 'all=True' parametresi bloklama hatalarını çözer. 
-# MUTLAKA diğer tüm importlardan önce gelmelidir.
+# 'all=True' parametresi Render'da aldığın bloklama hatasını çözen anahtardır.
+# MUTLAKA dosyanın en üstünde, diğer tüm kütüphanelerden önce gelmelidir.
 eventlet.monkey_patch(all=True)
 
 import os
@@ -12,16 +12,17 @@ from datetime import datetime
 
 # Uygulama Yapılandırması
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'fixed-chat-2026'
+app.config['SECRET_KEY'] = 'final-secure-key-2026'
 
-# Veritabanı Yolu (Render/SQLite uyumluluğu için tam yol)
+# Veritabanı Yolu (Render/SQLite uyumluluğu için absolute path)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'chat_data.db')
+db_path = os.path.join(basedir, 'chat_data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-# async_mode='eventlet' internet ortamı için zorunludur
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+# Render için eventlet modu zorunludur. cors_allowed_origins="*" bağlantı hatalarını önler.
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet', logger=False, engineio_logger=False)
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
@@ -34,6 +35,7 @@ class User(UserMixin, db.Model):
     avatar = db.Column(db.Text, nullable=True, default='')
 
 class Message(db.Model):
+    id = db.Column(#ff0000)
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
     content = db.Column(db.Text, nullable=False)
@@ -46,14 +48,17 @@ def load_user(user_id):
 
 # Rotalar
 @app.route('/')
-def index(): return redirect(url_for('login'))
+def index():
+    return redirect(url_for('login'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        uname, pwd = request.form.get('username'), request.form.get('password')
-        if not User.query.filter_by(username=uname).first():
-            db.session.add(User(username=uname, password=pwd))
+        uname = request.form.get('username')
+        pwd = request.form.get('password')
+        if uname and pwd and not User.query.filter_by(username=uname).first():
+            new_user = User(username=uname, password=pwd)
+            db.session.add(new_user)
             db.session.commit()
             return redirect(url_for('login'))
     return render_template('register.html')
@@ -63,40 +68,78 @@ def login():
     if request.method == 'POST':
         user = User.query.filter_by(username=request.form.get('username')).first()
         if user and user.password == request.form.get('password'):
-            login_user(user); return redirect(url_for('chat'))
+            login_user(user)
+            return redirect(url_for('chat'))
     return render_template('login.html')
 
 @app.route('/chat')
 @login_required
 def chat():
-    messages = Message.query.all()
+    all_messages = Message.query.all()
     history = []
-    for m in messages:
+    for m in all_messages:
         sender = User.query.filter_by(username=m.username).first()
         history.append({
             'id': m.id, 'username': m.username, 'content': m.content, 
-            'type': m.type, 'timestamp': m.timestamp, 'avatar': sender.avatar if sender else ''
+            'type': m.type, 'timestamp': m.timestamp, 
+            'avatar': sender.avatar if sender else ''
         })
     return render_template('chat.html', user=current_user, history=history)
 
 @app.route('/logout')
-def logout(): logout_user(); return redirect(url_for('login'))
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 # Socket Olayları
+active_users = {}
+
+@socketio.on('connect')
+def connect():
+    if current_user.is_authenticated:
+        active_users[request.sid] = {"name": current_user.username, "avatar": current_user.avatar or ''}
+        emit('user_list', list(active_users.values()), broadcast=True)
+
+@socketio.on('disconnect')
+def disconnect():
+    active_users.pop(request.sid, None)
+    emit('user_list', list(active_users.values()), broadcast=True)
+
 @socketio.on('message')
 def handle_msg(data):
+    if not current_user.is_authenticated: return
     now = datetime.now().strftime("%H:%M")
-    new_m = Message(username=current_user.username, content=data['msg'], type=data.get('type', 'text'), timestamp=now)
-    db.session.add(new_m); db.session.commit()
+    m_type = data.get('type', 'text')
+    new_m = Message(username=current_user.username, content=data['msg'], type=m_type, timestamp=now)
+    db.session.add(new_m)
+    db.session.commit()
+    
     emit('message', {
         'id': new_m.id, 'user': current_user.username, 'msg': data['msg'], 
-        'time': now, 'type': data.get('type', 'text'), 'avatar': current_user.avatar or ''
+        'time': now, 'type': m_type, 'avatar': current_user.avatar or ''
     }, broadcast=True)
+
+@socketio.on('delete_message')
+def delete_msg(data):
+    msg = Message.query.get(data['id'])
+    if msg and msg.username == current_user.username:
+        db.session.delete(msg)
+        db.session.commit()
+        emit('remove_message', {'id': data['id']}, broadcast=True)
+
+@socketio.on('update_avatar')
+def update_avatar(data):
+    user = User.query.get(current_user.id)
+    user.avatar = data['img']
+    db.session.commit()
+    for sid, info in active_users.items():
+        if info['name'] == user.username: info['avatar'] = data['img']
+    emit('user_list', list(active_users.values()), broadcast=True)
 
 # Sunucu Başlatma
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     port = int(os.environ.get("PORT", 5000))
-    # 'debug=False' ve 'log_output=False' bloklamayı önlemeye yardımcı olur
+    # 'log_output=False' Render loglarını temiz tutar ve bloklamayı azaltır.
     socketio.run(app, host='0.0.0.0', port=port, debug=False, log_output=False)
