f60b33b085608b3e85205dccd11b666a506cbc4a
bollulu <senol_erk_91@hotmail.com>
2026-01-15 23:43:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,21 +9,19 @@ from flask_socketio import SocketIO, emit, join_room
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'chat-ultra-v2026-final'
+app.config['SECRET_KEY'] = 'fixed-ultra-2026'
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'chat_final_v10.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'ultra_v12.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024 # 100MB Limit
+app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024 # 50MB Limit
 
 db = SQLAlchemy(app)
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent', max_http_buffer_size=100 * 1024 * 1024)
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent', max_http_buffer_size=50 * 1024 * 1024)
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+online_users = {}
 
-online_users = {} # {username: {avatar: str, sid: str}}
-
-# --- VERİTABANI MODELLERİ ---
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
@@ -36,13 +34,13 @@ class Message(db.Model):
     username = db.Column(db.String(50))
     user_avatar = db.Column(db.Text)
     content = db.Column(db.Text)
-    msg_type = db.Column(db.String(10)) # text, image, video, audio
+    msg_type = db.Column(db.String(10)) # text, image, audio
     timestamp = db.Column(db.String(10))
+    is_read = db.Column(db.Boolean, default=False)
 
 with app.app_context():
     db.create_all()
 
-# --- AUTH ---
 @login_manager.user_loader
 def load_user(user_id): return db.session.get(User, int(user_id))
 
@@ -70,7 +68,6 @@ def register():
 @login_required
 def chat(): return render_template('chat.html', user=current_user)
 
-# --- SOKET OLAYLARI ---
 @socketio.on('join')
 def on_join(data):
     room = data.get('room', 'Genel')
@@ -78,9 +75,8 @@ def on_join(data):
     session['room'] = room
     online_users[current_user.username] = {'avatar': current_user.avatar, 'sid': request.sid}
     emit('user_list', {u: info['avatar'] for u, info in online_users.items()}, broadcast=True)
-    
     msgs = Message.query.filter_by(room=room).order_by(Message.id.desc()).limit(50).all()
-    history = [{'id': m.id, 'user': m.username, 'avatar': m.user_avatar, 'msg': m.content, 'type': m.msg_type, 'time': m.timestamp} for m in reversed(msgs)]
+    history = [{'id': m.id, 'user': m.username, 'avatar': m.user_avatar, 'msg': m.content, 'type': m.msg_type, 'time': m.timestamp, 'is_read': m.is_read} for m in reversed(msgs)]
     emit('history', history)
 
 @socketio.on('message')
@@ -89,16 +85,15 @@ def handle_msg(data):
     now = datetime.now().strftime("%H:%M")
     msg = Message(username=current_user.username, user_avatar=current_user.avatar, content=data['msg'], msg_type=data.get('type', 'text'), room=room, timestamp=now)
     db.session.add(msg); db.session.commit()
-    emit('message', {'id': msg.id, 'user': current_user.username, 'avatar': current_user.avatar, 'msg': data['msg'], 'type': msg.msg_type, 'time': now}, to=room)
+    emit('message', {'id': msg.id, 'user': current_user.username, 'avatar': current_user.avatar, 'msg': data['msg'], 'type': msg.msg_type, 'time': now, 'is_read': False}, to=room)
 
-@socketio.on('delete_msg')
-def delete_msg(data):
-    msg = db.session.get(Message, data.get('id'))
-    if msg and msg.username == current_user.username:
-        db.session.delete(msg); db.session.commit()
-        emit('msg_deleted', {'id': data.get('id')}, broadcast=True)
+@socketio.on('mark_as_read')
+def mark_as_read(data):
+    room = data.get('room')
+    Message.query.filter_by(room=room, is_read=False).filter(Message.username != current_user.username).update({Message.is_read: True})
+    db.session.commit()
+    emit('messages_read_update', {'room': room}, to=room)
 
-# --- WEBRTC SIGNALLING ---
 @socketio.on('call-user')
 def call_user(data):
     target = online_users.get(data['to'])
@@ -113,12 +108,6 @@ def make_answer(data):
 def ice_candidate(data):
     target = online_users.get(data['to'])
     if target: emit('ice-candidate', {'candidate': data['candidate'], 'from': current_user.username}, to=target['sid'])
-
-@socketio.on('disconnect')
-def on_disconnect():
-    if current_user.is_authenticated and current_user.username in online_users:
-        del online_users[current_user.username]
-        emit('user_list', {u: info['avatar'] for u, info in online_users.items()}, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
