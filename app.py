416f5680823e5e237d1e402cb04b1489f6abe0d2
bollulu <senol_erk_91@hotmail.com>
2026-01-15 07:51:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ from flask_socketio import SocketIO, emit, join_room
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'sohbet-gevent-2026'
+app.config['SECRET_KEY'] = 'sohbetimiz-2026-final'
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'chat_final.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'final_v3.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -19,7 +19,6 @@ socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
-# Modeller
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
@@ -37,21 +36,17 @@ with app.app_context():
     db.create_all()
 
 @login_manager.user_loader
-def load_user(user_id):
-    return db.session.get(User, int(user_id))
+def load_user(user_id): return db.session.get(User, int(user_id))
 
-# Rotalar
 @app.route('/')
-def index():
-    return redirect(url_for('login'))
+def index(): return redirect(url_for('login'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         user = User.query.filter_by(username=request.form.get('username')).first()
         if user and user.password == request.form.get('password'):
-            login_user(user)
-            return redirect(url_for('chat'))
+            login_user(user); return redirect(url_for('chat'))
     return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -59,28 +54,22 @@ def register():
     if request.method == 'POST':
         u, p = request.form.get('username'), request.form.get('password')
         if u and p and not User.query.filter_by(username=u).first():
-            db.session.add(User(username=u, password=p))
-            db.session.commit()
+            db.session.add(User(username=u, password=p)); db.session.commit()
             return redirect(url_for('login'))
     return render_template('register.html')
 
 @app.route('/chat')
 @login_required
-def chat():
-    return render_template('chat.html', user=current_user)
+def chat(): return render_template('chat.html', user=current_user)
 
 @app.route('/live')
 @login_required
-def live():
-    # Burası 404 hatasını çözen yer!
-    return render_template('live.html', user=current_user)
+def live(): return render_template('live.html', user=current_user)
 
 @app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
+def logout(): logout_user(); return redirect(url_for('login'))
 
-# Socket Olayları
+# Socket İşlemleri
 @socketio.on('join')
 def on_join(data):
     room = data.get('room', 'Genel')
@@ -95,14 +84,32 @@ def handle_msg(data):
     room = session.get('room', 'Genel')
     now = datetime.now().strftime("%H:%M")
     msg = Message(username=current_user.username, content=data['msg'], room=room, timestamp=now)
-    db.session.add(msg)
-    db.session.commit()
-    emit('message', {'id': msg.id, 'user': current_user.username, 'msg': data['msg'], 'time': now}, to=room)
+    db.session.add(msg); db.session.commit()
+    emit('message', {'id': msg.id, 'user': current_user.username, 'msg': data['msg'], 'time': now, 'avatar': current_user.avatar}, to=room)
 
-# WebRTC Sinyalleşme (Canlı Yayın İçin)
+@socketio.on('delete_message')
+def delete_msg(data):
+    msg = db.session.get(Message, data['id'])
+    if msg and msg.username == current_user.username:
+        msg_id = msg.id
+        db.session.delete(msg); db.session.commit()
+        emit('remove_message', {'id': msg_id}, to=session.get('room'))
+
+@socketio.on('update_avatar')
+def update_avatar(data):
+    user = db.session.get(User, current_user.id)
+    user.avatar = data['img']
+    db.session.commit()
+
+# Canlı Yayın Sinyalleşme
+@socketio.on('join_live')
+def handle_join_live(data):
+    join_room('live_room')
+    emit('user_joined', room='live_room', include_self=False)
+
 @socketio.on('signal')
 def handle_signal(data):
-    emit('signal', data, to=data['to'], include_self=False)
+    emit('signal', data, room='live_room', include_self=False)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=10000)
