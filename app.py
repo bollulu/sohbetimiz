33497e279d4e3a677ca69517014eba3b957109f6
bollulu <senol_erk_91@hotmail.com>
2026-01-17 22:58:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,21 +8,22 @@ from flask_socketio import SocketIO, emit, join_room, leave_room
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'sohbetimiz_v6_final'
-app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 1024  # 1 GB Limit
+app.config['SECRET_KEY'] = 'sohbetimiz_ultra_v8'
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 1024 # 1 GB Limit
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'chat_final.db')
 
 db = SQLAlchemy(app)
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
 
-# --- STABİLİZE EDİLMİŞ MODELLER ---
+# --- MODELLER ---
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, index=True)
     password = db.Column(db.String(100))
     avatar = db.Column(db.Text)
+    gender = db.Column(db.String(10))
     blocked_users = db.Column(db.Text, default='[]')
 
 class Group(db.Model):
@@ -35,9 +36,8 @@ class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     room = db.Column(db.String(100), index=True)
     sender = db.Column(db.String(50))
-    sender_ava = db.Column(db.Text)
     content = db.Column(db.Text)
-    m_type = db.Column(db.String(20), default='text')
+    m_type = db.Column(db.String(20), default='text') # text, image, video, audio
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
 class Story(db.Model):
@@ -45,7 +45,7 @@ class Story(db.Model):
     username = db.Column(db.String(50))
     user_avatar = db.Column(db.Text)
     content = db.Column(db.Text)
-    media_type = db.Column(db.String(10))
+    media_type = db.Column(db.String(10)) # image/video
     audio_data = db.Column(db.Text, nullable=True)
     viewers = db.Column(db.Text, default='[]')
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
@@ -57,55 +57,49 @@ login_manager = LoginManager(app)
 @login_manager.user_loader
 def load_user(id): return db.session.get(User, int(id))
 
-# --- ANA ROTALAR ---
+# --- ROTALAR ---
 @app.route('/')
 def index():
-    if current_user.is_authenticated:
-        return redirect(url_for('chat'))
-    return render_template('auth.html')
+    return redirect(url_for('chat_page')) if current_user.is_authenticated else render_template('auth.html')
 
 @app.route('/chat')
 @login_required
-def chat():
+def chat_page():
     return render_template('chat.html', user=current_user)
+
+@app.route('/register', methods=['POST'])
+def register():
+    u, p = request.form.get('username'), request.form.get('password')
+    a, g = request.form.get('avatar_data'), request.form.get('gender')
+    if not User.query.filter_by(username=u).first():
+        new_u = User(username=u, password=p, avatar=a, gender=g)
+        db.session.add(new_u); db.session.commit()
+        login_user(new_u)
+    return redirect(url_for('chat_page'))
 
 @app.route('/login', methods=['POST'])
 def login():
     u, p = request.form.get('username'), request.form.get('password')
     user = User.query.filter_by(username=u, password=p).first()
-    if user: login_user(user); return redirect(url_for('chat'))
-    return redirect(url_for('/'))
-
-@app.route('/register', methods=['POST'])
-def register():
-    u, p, a = request.form.get('username'), request.form.get('password'), request.form.get('avatar_data')
-    if not User.query.filter_by(username=u).first():
-        new_u = User(username=u, password=p, avatar=a or "https://cdn-icons-png.flaticon.com/512/149/149071.png")
-        db.session.add(new_u); db.session.commit(); login_user(new_u)
-    return redirect(url_for('chat'))
+    if user: login_user(user)
+    return redirect(url_for('chat_page'))
 
 @app.route('/logout')
 def logout():
     logout_user(); return redirect(url_for('index'))
 
-@app.route('/delete_account', methods=['POST'])
-@login_required
-def delete_account():
-    db.session.delete(current_user); db.session.commit()
-    logout_user(); return redirect(url_for('index'))
-
-# --- SOCKET OLAYLARI ---
+# --- SOCKET ---
 @socketio.on('join')
 def on_join(data):
     join_room(data['room'])
-    msgs = Message.query.filter_by(room=data['room']).order_by(Message.timestamp.asc()).all()
-    emit('load_history', [{'sender':m.sender, 'ava':m.sender_ava, 'content':m.content, 'type':m.m_type} for m in msgs])
+    msgs = Message.query.filter_by(room=data['room']).all()
+    emit('load_history', [{'sender':m.sender,'content':m.content,'type':m.m_type} for m in msgs])
 
 @socketio.on('send_message')
 def handle_msg(data):
-    new_m = Message(room=data['room'], sender=current_user.username, sender_ava=current_user.avatar, content=data['msg'], m_type=data.get('type','text'))
-    db.session.add(new_m); db.session.commit()
-    emit('new_message', {'sender':current_user.username, 'ava':current_user.avatar, 'content':data['msg'], 'type':data.get('type','text'), 'room':data['room']}, room=data['room'])
+    m = Message(room=data['room'], sender=current_user.username, content=data['msg'], m_type=data.get('type','text'))
+    db.session.add(m); db.session.commit()
+    emit('new_message', {'sender':current_user.username, 'content':data['msg'], 'type':data.get('type','text'), 'room':data['room']}, room=data['room'])
 
 @socketio.on('post_story')
 def handle_story(data):
@@ -113,16 +107,13 @@ def handle_story(data):
     db.session.add(s); db.session.commit()
     send_stories()
 
-@socketio.on('get_stories')
 def send_stories():
     stories = Story.query.all()
     grouped = {}
     for s in stories:
         if s.username not in grouped: grouped[s.username] = {'avatar':s.user_avatar, 'items':[]}
-        grouped[s.username]['items'].append({'id':s.id, 'content':s.content, 'type':s.media_type, 'audio':s.audio_data, 'viewers':json.loads(s.viewers), 'username':s.username})
+        grouped[s.username]['items'].append({'id':s.id,'content':s.content,'type':s.media_type,'audio':s.audio_data,'username':s.username})
     emit('stories_update', grouped, broadcast=True)
 
-# (Diğer socket fonksiyonları: delete_story, toggle_block vb. buraya eklenebilir)
-
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app)
