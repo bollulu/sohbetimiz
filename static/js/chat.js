347d17a843e91f1f2f6bd8f7a839affa04c040fe
bollulu <senol_erk_91@hotmail.com>
2026-01-19 01:13:50 +0300

    Update chat.js

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,17 +1,16 @@
 const socket = io();
 
-socket.emit("join", { room: currentRoom });
-
-socket.on("message", data => {
-    const div = document.createElement("div");
-    div.className = "message " + (data.user === myUsername ? "me" : "other");
-    div.innerHTML = `<b>${data.user}</b><br>${data.msg}`;
-    document.getElementById("chat").appendChild(div);
-});
-
-function sendMessage() {
-    const input = document.getElementById("msg");
-    if (!input.value.trim()) return;
-    socket.emit("message", { room: currentRoom, msg: input.value });
-    input.value = "";
+function send() {
+  const msg = document.getElementById("msg").value;
+  socket.emit("send_message", {
+    username: USERNAME,
+    message: msg
+  });
+  document.getElementById("msg").value = "";
 }
+
+socket.on("receive_message", data => {
+  const p = document.createElement("p");
+  p.innerHTML = `<b>${data.username}:</b> ${data.message}`;
+  document.getElementById("chat").appendChild(p);
+});
