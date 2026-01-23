8e8311a20c0505d7acc6eaa3fefa3c0aea88a367
bollulu <senol_erk_91@hotmail.com>
2026-01-23 19:46:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask_socketio import SocketIO, emit
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'whatsapp_story_master_2026'
+app.config['SECRET_KEY'] = 'whatsapp_ultra_v4_2026'
 app.config['MAX_CONTENT_LENGTH'] = 200 * 1024 * 1024 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -18,7 +18,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'da
 db = SQLAlchemy(app)
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent', max_http_buffer_size=200 * 1024 * 1024)
 
-# MODELLER
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True)
@@ -38,9 +37,9 @@ class Story(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50))
     user_avatar = db.Column(db.Text)
-    content = db.Column(db.Text) # Resim/Video Base64
-    media_type = db.Column(db.String(20)) # 'image' veya 'video'
-    music = db.Column(db.Text, default="") # Müzik Base64 (opsiyonel)
+    content = db.Column(db.Text)
+    media_type = db.Column(db.String(20))
+    music = db.Column(db.Text, default="")
 
 with app.app_context():
     db.create_all()
@@ -55,14 +54,11 @@ def home():
     if current_user.is_authenticated: return redirect(url_for('chat'))
     return redirect(url_for('login'))
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        new_user = User(username=request.form['username'], password=request.form['password'], gender=request.form['gender'], avatar=request.form['avatar_data'])
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('login'))
-    return render_template('register.html')
+@app.route('/chat')
+@login_required
+def chat():
+    msgs = Message.query.all()
+    return render_template('chat.html', user=current_user, initial_msgs=msgs)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -73,16 +69,10 @@ def login():
             return redirect(url_for('chat'))
     return render_template('login.html')
 
-@app.route('/chat')
-@login_required
-def chat():
-    msgs = Message.query.all()
-    return render_template('chat.html', user=current_user, initial_msgs=msgs)
-
 @app.route('/logout')
 def logout(): logout_user(); return redirect(url_for('login'))
 
-# SOCKET EVENTLERI
+# SOCKET ETKİLEŞİMLERİ
 online_users = {}
 
 @socketio.on('connect')
@@ -91,12 +81,6 @@ def connect():
         online_users[current_user.username] = {"avatar": current_user.avatar}
         emit('user_status', online_users, broadcast=True)
         send_all_stories()
-
-@socketio.on('disconnect')
-def disconnect():
-    if current_user.is_authenticated:
-        online_users.pop(current_user.username, None)
-        emit('user_status', online_users, broadcast=True)
 
 @socketio.on('message')
 def handle_msg(data):
@@ -114,55 +98,28 @@ def delete_msg(data):
         db.session.commit()
         emit('msg_deleted', {'id': data['id']}, broadcast=True)
 
-@socketio.on('update_profile')
-def up_profile(data):
-    user = db.session.get(User, current_user.id)
-    user.avatar = data['avatar']
-    db.session.commit()
-    online_users[user.username]['avatar'] = data['avatar']
-    emit('profile_updated', {'user': user.username, 'avatar': data['avatar']}, broadcast=True)
-    emit('user_status', online_users, broadcast=True)
-
-@socketio.on('update_bg')
-def update_bg(data):
-    user = db.session.get(User, current_user.id)
-    user.bg_img = data['bg']
-    db.session.commit()
-
 @socketio.on('add_story')
 def add_story(data):
-    new_story = Story(
-        username=current_user.username,
-        user_avatar=current_user.avatar,
-        content=data['content'],
-        media_type=data['type'],
-        music=data.get('music', '') # Müzik alanı eklendi
-    )
-    db.session.add(new_story)
+    s = Story(username=current_user.username, user_avatar=current_user.avatar, 
+              content=data['content'], media_type=data['type'], music=data.get('music',''))
+    db.session.add(s)
     db.session.commit()
     send_all_stories()
 
 @socketio.on('delete_story')
 def delete_story(data):
-    story_id = data['id']
-    story = db.session.get(Story, story_id)
-    if story and story.username == current_user.username:
-        db.session.delete(story)
+    s = db.session.get(Story, data['id'])
+    if s and s.username == current_user.username:
+        db.session.delete(s)
         db.session.commit()
-        send_all_stories() # Hikayeleri güncellenmiş olarak tekrar gönder
+        send_all_stories()
 
 def send_all_stories():
-    stories = Story.query.order_by(Story.id.asc()).all() # Hikayeler eklenme sırasına göre gelsin
+    stories = Story.query.all()
     output = {}
     for s in stories:
-        if s.username not in output:
-            output[s.username] = {"avatar": s.user_avatar, "items": []}
-        output[s.username]["items"].append({
-            "id": s.id, # Hikaye silmek için id eklendi
-            "content": s.content, 
-            "type": s.media_type, 
-            "music": s.music
-        })
+        if s.username not in output: output[s.username] = {"avatar": s.user_avatar, "items": []}
+        output[s.username]["items"].append({"id": s.id, "content": s.content, "type": s.media_type, "music": s.music})
     emit('all_stories', output, broadcast=True)
 
 if __name__ == '__main__':
