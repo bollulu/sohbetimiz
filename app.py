b4e8a8be3d24cc13e911fd28ea645373ca535414
bollulu <senol_erk_91@hotmail.com>
2026-01-24 01:39:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from flask_socketio import SocketIO, emit
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'ultra_final_v2026_pro'
-app.config['MAX_CONTENT_LENGTH'] = 200 * 1024 * 1024 
+app.config['SECRET_KEY'] = 'ultra_whatsapp_fixed_final_2026'
+app.config['MAX_CONTENT_LENGTH'] = 200 * 1024 * 1024  # 200MB Limit
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
@@ -23,24 +23,24 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(100))
-    avatar = db.Column(db.Text) 
+    avatar = db.Column(db.Text)
     gender = db.Column(db.String(20))
-    blocked_list = db.Column(db.Text, default="") 
+    blocked_list = db.Column(db.Text, default="")
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sender = db.Column(db.String(50))
-    avatar = db.Column(db.Text)
+    avatar = db.Column(db.Text) # Gönderildiği andaki avatar (gerekirse güncellenecek)
     content = db.Column(db.Text)
-    msg_type = db.Column(db.String(20)) # text, image, audio
+    msg_type = db.Column(db.String(20)) # text, image, video, audio
     timestamp = db.Column(db.String(20))
 
 class Story(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50))
-    avatar = db.Column(db.Text) # Paylaşan kişinin o anki avatarı
-    content = db.Column(db.Text) # Hikaye resmi/videosu
-    media_type = db.Column(db.String(10)) 
+    avatar = db.Column(db.Text)
+    content = db.Column(db.Text)
+    media_type = db.Column(db.String(10)) # image/video
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
 with app.app_context():
@@ -48,6 +48,7 @@ with app.app_context():
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+
 @login_manager.user_loader
 def load_user(id): return db.session.get(User, int(id))
 
@@ -56,7 +57,7 @@ def load_user(id): return db.session.get(User, int(id))
 def register():
     if request.method == 'POST':
         u = request.form.get('username')
-        if User.query.filter_by(username=u).first(): return "Kullanıcı adı alınmış!", 400
+        if User.query.filter_by(username=u).first(): return "Bu isim alınmış!", 400
         new_u = User(
             username=u, 
             password=request.form.get('password'), 
@@ -100,15 +101,17 @@ def on_connect():
         emit('update_online', online_users, broadcast=True)
         
         # Geçmiş Mesajlar
-        messages = Message.query.all()
+        msgs = Message.query.all()
         history = []
-        for m in messages:
+        for m in msgs:
+            # Güncel avatarı user tablosundan çekelim ki eski kalmasın
+            u = User.query.filter_by(username=m.sender).first()
+            current_ava = u.avatar if u else m.avatar
             history.append({
-                'id': m.id, 'sender': m.sender, 'avatar': m.avatar, 
+                'id': m.id, 'sender': m.sender, 'avatar': current_ava, 
                 'content': m.content, 'type': m.msg_type, 'time': m.timestamp
             })
         emit('load_history', history)
-        
         send_stories()
 
 @socketio.on('disconnect')
@@ -139,6 +142,7 @@ def delete_message(data):
         db.session.delete(msg); db.session.commit()
         emit('msg_deleted', {'id': data['id']}, broadcast=True)
 
+# HİKAYE EKLEME
 @socketio.on('add_story')
 def add_story(data):
     s = Story(username=current_user.username, avatar=current_user.avatar, 
@@ -153,21 +157,38 @@ def delete_story(data):
         db.session.delete(s); db.session.commit()
         send_stories()
 
+# PROFİL GÜNCELLEME (HER YERDE DEĞİŞSİN DİYE)
 @socketio.on('update_avatar')
 def update_avatar_func(data):
     current_user.avatar = data['avatar']
     db.session.commit()
+    
     online_users[current_user.username]['avatar'] = data['avatar']
+    
+    # 1. Online Listesini Güncelle
     emit('update_online', online_users, broadcast=True)
+    
+    # 2. Özel Event Gönder: "Bu kullanıcının avatarı değişti, herkes yenilesin"
+    emit('force_avatar_update', {
+        'username': current_user.username,
+        'new_avatar': data['avatar']
+    }, broadcast=True)
+    
+    # 3. Hikayeleri de güncelle ki oradaki kafa resmi değişsin
+    send_stories()
 
 def send_stories():
     stories = Story.query.all()
-    # Hikayeleri Kullanıcıya Göre Grupla
+    # User tablosundan en güncel avatarları alarak birleştir
     grouped = {}
     for s in stories:
+        # Veritabanından o kullanıcının güncel fotosunu bul
+        u = User.query.filter_by(username=s.username).first()
+        real_avatar = u.avatar if u else s.avatar
+        
         if s.username not in grouped: grouped[s.username] = []
         grouped[s.username].append({
-            'id': s.id, 'username': s.username, 'avatar': s.avatar, 
+            'id': s.id, 'username': s.username, 'avatar': real_avatar, 
             'content': s.content, 'type': s.media_type
         })
     emit('all_stories', grouped, broadcast=True)
